b3822390de0bc5272370443368e2ec29706dcbac
Filippo0420 <fpawelec0@gmail.com>
2022-11-08 09:29:11 +0100

    Dodaj pliki projektów.

---
+++ LibraryApp/DAO/LoansDAO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LibraryApp.DAO
+{
+    class LoansDAO
+    {
+        void connectToDatabase()
+        {
+            String str = "Server=localhost\\SQLEXPRESS2019;Integrated Security=True;Database=StudentsDatabase";
+
+            SqlConnection con = new SqlConnection(str);
+            try
+            {
+                con.Open();
+                MessageBox.Show("connected with sql server");
+            }
+            catch (Exception es)
+            {
+                MessageBox.Show(es.Message);
+            }
+        }
+    }
+}
---
+++ LibraryApp/Models/Loans.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp
+{
+    class Loans
+    {
+        int Id { get; set; }
+        int BookId { get; set; }
+        int CustomerId { get; set; }
+        DateTime LoanDate { get; set; }
+        DateTime ReturnDate { get; set; }
+    }
+}
